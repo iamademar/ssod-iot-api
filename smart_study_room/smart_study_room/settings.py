528ef0f67c71c92a6ccf241a71c66823fc5d7178
Ademar Tutor <ademartutor@gmail.com>
2024-09-04 09:09:40 +1200

    update settings.py

--- smart_study_room/smart_study_room/settings.py
+++ smart_study_room/smart_study_room/settings.py
@@ -33,7 +33,7 @@ API_KEY = os.getenv('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.27.174.228', 'localhost', '127.0.0.1']
 
 
 # Application definition
