b9b1385d5e7fc12d557b93d1d3c796e767f49b04
Ademar Tutor <ademartutor@gmail.com>
2024-09-04 08:30:46 +1200

    update settings.py

--- smart_study_room/smart_study_room/settings.py
+++ smart_study_room/smart_study_room/settings.py
@@ -83,13 +83,26 @@ WSGI_APPLICATION = 'smart_study_room.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smart_study_room',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }}
+if os.getenv('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'smart_study_room',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
