fb384cf8ae427a47ce69b5cfd640fe9dc84239e5
Ademar Tutor <ademartutor@gmail.com>
2024-09-04 08:37:18 +1200

    test print

--- smart_study_room/smart_study_room/settings.py
+++ smart_study_room/smart_study_room/settings.py
@@ -154,3 +154,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+
+print("DJANGO_ENV:", os.getenv('DJANGO_ENV'))
+print("DB_NAME:", os.getenv('DB_NAME'))
+print("DB_USER:", os.getenv('DB_USER'))
+print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
+print("DB_HOST:", os.getenv('DB_HOST'))
+print("DB_PORT:", os.getenv('DB_PORT'))
