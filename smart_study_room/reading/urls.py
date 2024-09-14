e43cb7756080064baba3b68ed87e0f4a79867e28
Ademar Tutor <ademartutor@gmail.com>
2024-09-14 20:46:42 +1200

    update on urls

--- smart_study_room/reading/urls.py
+++ smart_study_room/reading/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import create_reading, get_latest_readings
 
 urlpatterns = [
-    path('create/', create_reading, name='create_reading'),
-    path('latest/', get_latest_readings, name='get_latest_readings'),
+    path('create/', create_reading),
+    path('latest/', get_latest_readings),
 ]
