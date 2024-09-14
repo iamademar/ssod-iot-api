19a663c3d0e6471be7fe40dfb347a123f332b3af
Ademar Tutor <ademartutor@gmail.com>
2024-09-14 16:33:56 +1200

    update on reading logs

--- smart_study_room/reading/urls.py
+++ smart_study_room/reading/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_reading
+from .views import create_reading, get_latest_readings
 
 urlpatterns = [
     path('create/', create_reading, name='create_reading'),
+    path('latest/', get_latest_readings, name='get_latest_readings'),
 ]
