4878690c22db6750d0f1bd76712baa9312da3bd0
Ademar Tutor <ademartutor@gmail.com>
2024-09-14 21:07:30 +1200

    test sensor log removal

--- smart_study_room/occupancy/urls.py
+++ smart_study_room/occupancy/urls.py
@@ -4,7 +4,7 @@ from .views import save_room_occupancy, check_room_occupancy, get_latest_occupan
 urlpatterns = [
     path('sensor', save_room_occupancy),  # URL endpoint to save room occupancy data
     path('check_occupancy/<str:room_name>/', check_room_occupancy),  # URL endpoint to check room occupancy
-    path('sensor/logs', get_latest_occupancy_logs),  # URL endpoint to get latest occupancy logs
+    # path('sensor/logs', get_latest_occupancy_logs),  # URL endpoint to get latest occupancy logs
     path('temperature/<str:room_name>/', get_latest_room_temperature),  # URL endpoint to get temperature
     path('reading/logs', get_reading_logs),  # URL endpoint to get reading logs
 ]
