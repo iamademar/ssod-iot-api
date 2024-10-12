c86ae10a04af287c78255db2d91ca302df6368f2
Ademar Tutor <ademartutor@gmail.com>
2024-10-12 17:04:12 +1300

    update on main

--- smart_study_room/smart_study_room/settings.py
+++ smart_study_room/smart_study_room/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 API_KEY = os.getenv('API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['3.27.174.228', 'localhost', '127.0.0.1', 'iot.ademartutor.com']
 CORS_ALLOWED_ORIGINS = [
